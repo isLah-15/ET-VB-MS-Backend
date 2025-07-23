b310a1c876e3d68bb89f64ee7508317305a8939d
isLah-15 <alexiskish4146@gmail.com>
2025-07-24 00:11:32 +0300

    Completed the backend

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,35 @@ import event from './Event/event.router';
 import payment from './Payment/payment.router';
 import support from './Support/support.router';
 import booking from './Booking/booking.router';
+import cors from 'cors';
 
-const app = express();
 
-// middleware
-app.use(express.json())
+const initilizeApp = () => {
 
-// routes
-user(app);
-venue(app);
-event(app);
-payment(app);
-support(app);
-booking(app);
+  const app = express();
+    app.use(express.json()); //used to parse JSON bodies
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+    app.use(cors({
+    origin:'http://localhost:8081',
+    credentials: true
+    }));
 
-app.listen(8081, () => {
-  console.log('Server is running on http://localhost:8081');
-});
+    // routes
+    user(app);
+    venue(app);
+    event(app);
+    payment(app);
+    support(app);
+    booking(app);
 
+
+    app.get('/', (req, res) => {
+        res.send('Hello, World!');
+    })
+
+    return app;
+
+}
+
+const app = initilizeApp();
 export default app;
